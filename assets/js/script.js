1c8b70aa987cd51e0b3bb44ec0719f0ef9ec151e
Deepa <kosalai17@gmail.com>
2021-05-19 23:46:37 -0700

    Display user input in form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
-var formEl = document.querySelector("#task-form");
-var tasksTodoEl = document.querySelector("#tasks-to-do");
-console.log(formEl);
+var formEl = document.querySelector("#task-form"); //the input form
+var tasksTodoEl = document.querySelector("#tasks-to-do");// the id of ul
 
 var createTaskHandler=function(event){ 
     event.preventDefault();
     
+    // Assign the user input task name to TaskNameInput
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    
+    //Assign thev task type chosen to task type input 
+    var taskTypeInput =document.querySelector("select[name='task-type']").value;
+
+    // create a task info that contains both task name and task type 
+    var taskInfoEl= document.createElement("div");
+    taskInfoEl.className="task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    //create  a list item 
     var listItemEl = document.createElement("li");
-    listItemEl.textContent ="A new task";
     listItemEl.className = "task-item";
+   
+    
+    // Add the task info to <li>
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add the li to <ul>
     tasksTodoEl.appendChild(listItemEl);
+
 };
 formEl.addEventListener("submit",createTaskHandler);
 
